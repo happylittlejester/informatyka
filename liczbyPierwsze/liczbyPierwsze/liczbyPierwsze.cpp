9b188bb5467eafcbe9c3af482bb519c4d857ff64
Natalia Obst <obst.natalia@gmail.com>
2022-03-23 11:07:08 +0100

    Liczby pierwsze

---
+++ liczbyPierwsze/liczbyPierwsze/liczbyPierwsze.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n = 0;
+	int lp = 0;
+	int p = 2;
+
+
+	cout << "Ile liczb pierwszych chcesz wyswietlic? ";
+	cin >> n;
+
+	while (lp < n)
+	{
+		for (int d = 2; d < n; d++)
+		{
+			n% d;
+			if (n % d == 0)
+			{
+				cout << n << "nie jest liczba pierwsza" << endl;
+			}
+			else
+			{
+				cout << n << "jest liczba pierwsza" << endl;
+			}
+		}
+
+		lp += 1;
+	}
+}
