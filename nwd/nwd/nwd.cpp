e589a53738520c9aca40175b740811fc56aeb93a
Natalia Obst <obst.natalia@gmail.com>
2022-04-01 09:59:35 +0200

    algorytm nwd i nww

---
+++ nwd/nwd/nwd.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//algorytm euklidesa
+	int a = 1;
+	int b = 1;
+	int nww;
+
+	cout << "podaj a: ";
+	cin >> a;
+	cout << "podaj b: ";
+	cin >> b;
+
+	int iloczyn = a * b;
+	
+	while (a != b)
+	{
+		if (a < b)
+		{
+			b -= a;
+		}
+		else
+		{
+			a -= b;
+		}
+
+		nww = iloczyn / a;
+	}
+
+	cout << "najwieszky wspolny dzielnik to: " << a << endl;
+	cout << "najmnijsza wspolna wielokrotnosc to: " << nww << endl;
+}
