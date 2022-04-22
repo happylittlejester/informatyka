be551e6a72631af4858dfa90eeb9c554b3949178
Natalia Obst <obst.natalia@gmail.com>
2022-04-22 09:41:01 +0200

    ciag fibonacciego

---
+++ fibonacci/fibonacci/fibonacci.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int f;
+    int f0 = 0;
+    int f1 = 1;
+
+    cout << "Ile wyrazow ciagu wypisac: ";
+    cin >> n;
+
+    for (int i = 0; i <= n; i++)
+    {
+        if (i > 1)
+        {
+            f = f0 + f1;
+            f0 = f1;
+            f1 = f;
+        }
+        else
+        {
+            f = i;
+        }
+
+        cout << f << endl;
+    }
+}
