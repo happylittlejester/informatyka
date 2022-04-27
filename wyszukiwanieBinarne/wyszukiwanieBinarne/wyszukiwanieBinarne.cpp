d8d6821c90c914b6a4f1db33806ba69e026fffc5
Natalia Obst <obst.natalia@gmail.com>
2022-04-27 10:56:39 +0200

    wyszukiwanieBinarne

---
+++ wyszukiwanieBinarne/wyszukiwanieBinarne/wyszukiwanieBinarne.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int szukana;
+	int tab[15] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 };
+	int pozycja;
+
+	cout << "Podaj liczbe ktora chcesz znalezc: ";
+	cin >> szukana;
+
+	int l = 0;
+	int p = 15;
+	int sr = (l + p) / 2;
+
+	while (l <= p)
+	{
+		if (tab[sr] == szukana)
+		{
+			cout << "liczba " << szukana << " wystepuje w zbiorze w komorce o indeksie " << sr << endl;
+			break;
+		}
+		else if (tab[sr] > szukana)
+		{
+			p = sr - 1;
+		}
+		else
+		{
+			l = sr + 1;
+		}
+
+		sr = (l + p) / 2;
+	}
+}
